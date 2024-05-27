120f63b7ad9807fb7befc058abe88120c602323c
rnktlhr <rinakitluhur10@gmail.com>
2024-05-27 15:01:44 +0700

    menambahkan class orang dengan modifire, member dan methode

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class orang{
+    public: 
+        int umur;
 
+        orang(int pUmur) :
+            umur(pUmur){
+                cout << "Orang dibuat dengan umur" << umur << "\n" << endl;
+            }
+};
 
 int main(){
 
