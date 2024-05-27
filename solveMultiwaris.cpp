dad9cb249787d7ff838b717fe36190c977187c7d
rnktlhr <rinakitluhur10@gmail.com>
2024-05-27 15:18:22 +0700

    menambahkan class pekerja dengan akases modifire dan methode

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -11,6 +11,14 @@ class orang{
             }
 };
 
+class pekerja:public orang{
+    public:
+        pekerja(int pUmur) :
+            orang(pUmur){
+                cout << "Pekerja dibuat\n" << endl;
+            }
+};
+
 int main(){
 
 }
