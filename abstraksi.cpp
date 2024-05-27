84d1cb0b1dc8f9079baf519be816a7c09daa375c
rnktlhr <rinakitluhur10@gmail.com>
2024-05-27 15:38:48 +0700

    menambahkan class abstraksikls dengan member akses modifire dan methode

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class AbstarksiKlas{
+    private: string x, y;
+    public:
+        void setXY(string a, string b){
+            x = a;
+            y = b;
+        }
+        void display(){
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
+        }
+};
 
 
 int main(){
